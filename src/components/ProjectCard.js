6633c9eb73b9779850b2f041233349d3539dc1b5
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-06-30 15:29:09 -0300

    Adding presentation video

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <a target="_blank" href={link}>
+      <a target="_blank" rel="noreferrer" href={link}>
         <div className="proj-imgbx">
           <img src={imgUrl} alt={title}/>
           <div className="proj-txtx">
