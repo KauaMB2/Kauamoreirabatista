f295eea29860517a59f76edd8ced81e2ad125500
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-07-10 06:45:37 -0300

    Adding João Francisco comment

--- src/components/JSON/commentsData.js
+++ src/components/JSON/commentsData.js
@@ -1,5 +1,6 @@
 import cauafrancoPicture from "../../assets/img/comments/cauafranco.png"
 import fabiohenriquePicture from "../../assets/img/comments/fabiohenrique.png"
+import joaofrancisco from "../../assets/img/comments/joaofrancisco.png"
 
 export const commentsData=[
         {
@@ -15,5 +16,12 @@ export const commentsData=[
           link: 'https://www.instagram.com/fabiohenriquefh/',
           arrouba: 'fabiohenriquefh',
           content: 'Um bom profissional, aonde todas as demandas e extra demandas em que lhe foi atribuído, correspondeu acima das metas. Também é uma pessoa fácil de trabalhar, aonde aceita ideias e sempre haviam discussões saudáveis para a melhor entrega do projeto.',
+        },
+        {
+          avatar: joaofrancisco,
+          name: 'João Francisco',
+          link: 'https://www.instagram.com/joaumtorres/',
+          arrouba: 'joaumtorres',
+          content: '“VsMeter”, projeto de 2021, consistiu na criação de uma roupa de mensuração de sinais vitais, principalmente orquestrado por Kauã, que teve um desempenho excepcional. Ele demonstrou uma grande capacidade de encontrar soluções eficazes, atuando como o verdadeiro líder do grupo. Sempre à frente, estava inteirado de todas as ramificações do projeto, guiando o restante da equipe com segurança e precisão. Sua assertividade e habilidade em gerenciar o projeto foram fundamentais para o sucesso do mesmo. O sucesso da equipe pode ser creditado à sua capacidade de conciliar cada participante em uma função de relevância para cada um.',
         }
       ]
